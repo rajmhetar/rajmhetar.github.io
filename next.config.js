f6991ae619a202640978a2e466c974e7acacd9c1
Raj Mhetar <rajmhetar@gmail.com>
2025-01-23 20:58:17 -0600

    Update for static export with output: 'export'

--- next.config.js
+++ next.config.js
@@ -11,6 +11,8 @@ const nextConfig = {
   },
   basePath: isProd ? '/rajmhetar.github.io' : '',
   assetPrefix: isProd ? '/rajmhetar.github.io/' : '',
+  trailingSlash: true, // Ensures trailing slashes for GitHub Pages
+  output: 'export', // Enables static export
 };
 
 module.exports = nextConfig;
